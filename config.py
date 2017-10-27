fd34270631aa909ce9147be3eb80e5ff5304e78a
Peter <pndungu54@gmail.com>
2017-10-27 17:43:43 +0300

    add postgre configuration

--- config.py
+++ config.py
@@ -4,24 +4,31 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    
+    """Global configuration settings"""
     SECRET_KEY = 'You cannot hack this site fool'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    password = 'pN75216895#'
+    username = 'xcixor'
+    database = 'yummy_recipes'
+
 
     @staticmethod
     def init_app(app):
         pass
 
 class Development(Config):
+    """Settings used for development"""
     DEBUG = True
-    # SQLALCHEMY_DATABASE_URI = postgresql://xcixor:pN75216895@hostname/database
+    SQLALCHEMY_DATABASE_URI = 'postgresql://xcixor:pN75216895#@localhost/yummy_recipes'
 
 
 class Testing(Config):
+    """Settings used for testing"""
     TESTING = True
 
 class Production(Config):
+    """Settings used for production"""
     DEBUG = False
 
 config = {
