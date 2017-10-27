d8aafb0a0d509051bca1adfe9e2400133102bfa2
Peter <pndungu54@gmail.com>
2017-10-27 17:44:23 +0300

    Import SQLAlchemy

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,32 +1,16 @@
-"""The purpose of this constructor is to set up the application
+"""The purpose of this constructor is to set up the api
 by importing the necessary modules for the app to run
 Methods:
     create_app: Serves as the app factory
 """
 from flask import Flask
 
-from flask_bootstrap import Bootstrap
-
-from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
 from config import config
 
-from .app_classes import User, Category, Recipe, UserManager
+db = SQLAlchemy()
 
-usr_mgr = UserManager()
-
-myuser = User()
-
-category = Category()
-
-recipe = Recipe()
-
-bootstrap = Bootstrap()
-
-
-login_manager = LoginManager()
-login_manager.session_protection = 'strong'
-login_manager.login_view = 'home.login'
 
 
 def create_app(configuration):
@@ -36,14 +20,7 @@ def create_app(configuration):
     app.config.from_object(config[configuration])
     config[configuration].init_app(app)
 
-    bootstrap.init_app(app)
-    login_manager.init_app(app)
-
-    from .home import home as home_blueprint
-    app.register_blueprint(home_blueprint)
-
-    from .authentication import authentication as authentication_blueprint
-    app.register_blueprint(authentication_blueprint, url_prefix='/authentication')
+    db.init_app(app)
 
     return app
 
