8c19628183ffb293768070edad9a2ec0a8629c49
Peter <pndungu54@gmail.com>
2017-10-27 21:14:59 +0300

    Add shell context for db and database models

--- manage.py
+++ manage.py
@@ -2,22 +2,19 @@ from myapp import create_app, db
 
 from flask_script import Manager, Shell
 
-app = create_app('default')
+from myapp.models import User, Recipe, Category
+
+app = create_app('development')
 
 manager = Manager(app)
 
 def make_shell_context():
-    """Creates the custom context for the python shell"""
-    return dict(app=app)
+    """Automatically import object instances in the shell context"""
+    return dict(app=app, db=db, User=User, Category=Category, Recipe=Recipe)
+manager.add_command("shell", Shell(make_context=make_shell_context))
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
-@manager.command
-def test():
-    """Run the unit tests."""
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == '__main__':
     manager.run()
