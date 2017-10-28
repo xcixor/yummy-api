efbcc2ae9f87882815b7be0de1e086e897e24838
Peter <pndungu54@gmail.com>
2017-10-28 18:43:50 +0300

    Add date_create and date_modified timestamps to all tables

--- myapp/models.py
+++ myapp/models.py
@@ -10,6 +10,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), nullable=False, index=True)
     password = db.Column(db.String(256), nullable=False, unique=True)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), \
+    onupdate=db.func.current_timestamp())
     categories = db.relationship('Category', backref='user')
 
     def save(self):
@@ -25,18 +28,24 @@ class Category(db.Model):
     __tablename__ = 'categories'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), nullable=False, index=True)
-    description = db.Column(db.String(256))
+    description = db.Column(db.String(255))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    recipes = db.relationship('Recipe', backref = 'category')
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), \
+    onupdate=db.func.current_timestamp())
+    recipes = db.relationship('Recipe', backref='category')
 
     def __repr__(self):
         return '<Category: {}, Description: {}>'.format(self.name, self.description)
 class Recipe(db.Model):
     """Defines the recipe table"""
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256), nullable=False, unique=True)
-    ingredients = db.Column(db.String(256))
-    preparation = db.Column(db.String(256))
+    name = db.Column(db.String(255), nullable=False, unique=True)
+    ingredients = db.Column(db.String(255))
+    preparation = db.Column(db.String(255))
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), \
+    onupdate=db.func.current_timestamp())
     category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
 
     def __repr__(self):
