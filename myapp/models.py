029bc1f273f169bf58d2478df5a155c2554a1769
Peter <pndungu54@gmail.com>
2017-10-28 20:48:54 +0300

    Add columns for storing datestamps in models

--- myapp/models.py
+++ myapp/models.py
@@ -15,12 +15,29 @@ class User(db.Model):
     onupdate=db.func.current_timestamp())
     categories = db.relationship('Category', backref='user')
 
+    def __init__(self, username, password):
+        """Creates a user with a username and password"""
+        self.username = username
+        self.password = password
+        
+
     def save(self):
         """Saves a user to the database"""
         db.session.add(self)
         db.session.commit()
 
+    @staticmethod
+    def find_user(username):
+        """Find a user based on username"""
+        return User.query.filter_by(username=username).first()
+
+    def delete(self):
+        """Deletes a user from the database"""
+        db.session.delete(self)
+        db.session.commit()
+
     def __repr__(self):
+        """Returns a string representation of the user"""
         return '<User: {}>'.format(self.username)
 
 class Category(db.Model):
@@ -35,6 +52,24 @@ class Category(db.Model):
     onupdate=db.func.current_timestamp())
     recipes = db.relationship('Recipe', backref='category')
 
+    def __init__(self, name, description, user_id):
+        """Creates a category with the provided name, description and owner"""
+        self.description = description
+        self.name = name
+        self,user_id = user_id
+        
+
+    def save(self):
+        """Saves a category to the database"""
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        """Deletes a user from the database"""
+        db.session.delete(self)
+        db.session.commit()
+
+
     def __repr__(self):
         return '<Category: {}, Description: {}>'.format(self.name, self.description)
 class Recipe(db.Model):
@@ -48,8 +83,26 @@ class Recipe(db.Model):
     onupdate=db.func.current_timestamp())
     category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
 
+    def __init__(self, name, ingredients, preparation, category_id):
+        """Creates recipe with the provided attributes"""
+        self.name = name
+        self.ingredients = ingredients
+        self.preparation = preparation
+        self.category_id = category_id
+
+    def save(self):
+        """Saves a recipe to the database"""
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        """Deletes a recipe from the database"""
+        db.session.delete(self)
+        db.session.commit()
+
     def __repr__(self):
-        return '<Recipe: {}, Preparation: {}>'.format(self.name, self.preparation)
+        """Returns a string representation of the recipe"""
+        return '<Recipe: {}>'.format(self.name)
 
 
 
