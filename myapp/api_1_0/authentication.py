0e4cc3d0f41d991875f85084b1a960b1ae9faa22
Peter <pndungu54@gmail.com>
2017-10-28 18:22:33 +0300

    Create authentication module and add registration functionality

---
+++ myapp/api_1_0/authentication.py
@@ -0,0 +1,59 @@
+"""Defines the api's routes for registration"""
+import re
+
+from . import api
+
+from flask import request, jsonify, make_response
+
+from ..models import User
+
+from ..models import User
+
+def validate_name(username):
+    """Validates whether the username has special characters"""
+    if re.match("^[a-zA-Z0-9 _]*$", username):
+        return True
+    return False
+
+
+@api.route('/auth/register',  methods=['POST'])
+def register():
+    """Registers new users"""
+    if request.method == 'POST':
+        # username = str(request.data.get('username', ''))
+        username = request.json.get('username')
+        password = request.json.get('password')
+        confirm_password = request.json.get('confirm_password')
+        # password = str(request.data.get('password', ''))
+        # confirm_password = str(request.data.get('confirm_password', ''))
+        # username = {'name': request.json['username']}
+        # password = {'password': request.json['password']}
+        # confirm_password = {'confirm_password': request.json['confirm_password']}
+
+        if username and password and confirm_password:
+            valid_username = validate_name(username)
+            if not valid_username:
+                response = {'message': 'Username cannot contain special characters except for underscores'}
+                return make_response(jsonify(response)), 400
+            if len(username)< 6:
+                response = {'message': 'Username cannot be less than six characters'}
+                return make_response(jsonify(response)), 400
+
+            if password != confirm_password:
+                response = {'message': 'Passwords do not match'}
+                return make_response(jsonify(response)), 400
+
+            reg_user = User.query.filter_by(username=username).first()
+            if not reg_user:
+                # data = request.data
+                # username = data['username']
+                # password = data['password']
+                user = User(username=username, password=password)
+                user.save()
+                response = {'message': 'Registered successfully'}
+                return make_response(jsonify(response)), 400
+            response = {'message': 'That user already exist'}
+            return make_response(jsonify(response)), 202
+        response = {'message': 'Please provide all required information'}
+        return make_response(jsonify(response)), 400
+
