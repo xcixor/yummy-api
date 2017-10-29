71923a06cfd9e36ba83a481c956d2a488875f8d9
Peter <pndungu54@gmail.com>
2017-10-29 23:09:31 +0300

    Use json dumps to fix post error

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -4,11 +4,13 @@ sys.path.append("..")
 
 import unittest
 
-from flask import current_app, jsonify
+from flask import current_app
 
 from myapp import create_app, db
 
 import json
+
+from myapp.models import User
 
 
 class UserTestCase(unittest.TestCase):
@@ -19,17 +21,16 @@ class UserTestCase(unittest.TestCase):
     def setUp(self):
         """Set up testing environment variables"""
         self.app = create_app('testing')
-        self.client = self.app.test_client
-        #bind app to current context
         self.app_context = self.app.app_context()
         self.app_context.push()
+        self.client = self.app.test_client
         #create database tables
         db.create_all()
         self.client = self.app.test_client
         self.user1 = {
             'username': 'mrpeter',
-            'password': 'pass1',
-            'confirm_password': 'pass1'
+            'password': 'pass123',
+            'confirm_password': 'pass123'
         }
         self.user2 = {
             'username': 'mr##ndungu',
@@ -47,9 +48,9 @@ class UserTestCase(unittest.TestCase):
             'confirm_password': 'passtwo'
         }
         self.user5 = {
-            'username': 'mrpeterndungu',
-            'password': 'pass1',
-            'confirm_password': 'pass1'
+            'username': 'mrndungu',
+            'password': 'passone',
+            'confirm_password': 'passone'
         }
         self.headers = {'Content-type': 'application/json'}
         self.url = '/auth/register'
@@ -62,27 +63,27 @@ class UserTestCase(unittest.TestCase):
 
     def test_username_validity(self):
         """Tests whether the username provided contains any special chatacters except underscores"""
-        result = self.client().post(self.url, data=self.user1, headers=self.headers)
+        result = self.client().post(self.url, data=json.dumps(self.user2), headers=self.headers)
         self.assertEqual(result.status_code, 400)
 
     def test_password_length(self):
-        result = self.client().post(self.url, data=self.user3, headers=self.headers)
+        result = self.client().post(self.url, data=json.dumps(self.user3), headers=self.headers)
         self.assertAlmostEqual(result.status_code, 400)
 
     def test_password_equal_confirmpassword(self):
         """Tests whether the confirmation password supplied matches the actual password"""
-        result = self.client().post(self.url, data=self.user4, headers=self.headers)
+        result = self.client().post(self.url, data=json.dumps(self.user4), headers=self.headers)
         self.assertEqual(result.status_code, 400)
 
     def test_user_already_exists(self):
         """Tests whether registering user already exists"""
-        self.client().post(self.url, data=self.user1, headers=self.headers)
-        result = self.client().post(self.url, data=self.user1, headers=self.headers)
-        self.assertEqual(result.status_code, 400)
+        self.client().post(self.url, data=json.dumps(self.user1), headers=self.headers)
+        result = self.client().post(self.url, data=json.dumps(self.user1), headers=self.headers)
+        self.assertEqual(result.status_code, 202)
 
     def test_register_user(self):
         """Tests whether a user can be registered successfully"""
-        result = self.client().post(self.url, data=self.user5, headers=self.headers)
+        result = self.client().post(self.url, data=json.dumps(self.user5), headers=self.headers)
         self.assertEqual(result.status_code, 201)
         
 
