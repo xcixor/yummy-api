cba9efb99c8921a8185e5ad6459c9e2ab3775419
Peter <pndungu54@gmail.com>
2017-10-29 20:42:33 +0300

    Specify database for testing

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     SECRET_KEY = 'You cannot hack this site fool'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
     @staticmethod
     def init_app(app):
@@ -23,7 +23,7 @@ class Development(Config):
 class Testing(Config):
     """Settings used for testing"""
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql:///yummy_recipes'
+    SQLALCHEMY_DATABASE_URI = 'postgresql:///test_yummy'
 
 class Production(Config):
     """Settings used for production"""
