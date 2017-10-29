e31dd4a63c37f39120ea1ba3116e1fffa0fe527f
Peter <pndungu54@gmail.com>
2017-10-29 18:14:47 +0300

    Remove unnecessary shell context

--- manage.py
+++ manage.py
@@ -1,8 +1,6 @@
 from myapp import create_app, db
 
-from flask_script import Manager, Shell
-
-from myapp.models import User, Recipe, Category
+from flask_script import Manager
 
 from flask_migrate import Migrate, MigrateCommand
 
@@ -12,13 +10,7 @@ manager = Manager(app)
 
 migrate = Migrate(app, db)
 
-def make_shell_context():
-    """Automatically import object instances in the shell context"""
-    return dict(app=app, db=db, User=User, Category=Category, Recipe=Recipe)
-manager.add_command("shell", Shell(make_context=make_shell_context))
-
 manager.add_command('db', MigrateCommand)
-
 
 if __name__ == '__main__':
     manager.run()
